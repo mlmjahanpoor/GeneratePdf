6d3f777a1a7efc94e6776ac74af96bad6e24aa54
mlmjahanpoor <mlmjahanpoor@gmail.com>
2021-12-17 16:55:11 +0330

    init by moslem jahanpoor

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using CreateFactor.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Wkhtmltopdf.NetCore;
+
+namespace CreateFactor.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        private readonly IGeneratePdf _generatePdf;
+        public HomeController(IGeneratePdf generatePdf, IWebHostEnvironment environment)
+        {
+            _generatePdf = generatePdf;
+            _environment = environment;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var root = _environment.WebRootPath;
+            var PdfBytes = _generatePdf.GetPDF("<h1>This is my title!</h1>");
+            Utilities utilities = new Utilities(_environment);
+            utilities.ByteArrayToFile("factor1.pdf",PdfBytes);
+
+            return Ok(new { IsSuccess = true });
+        }
+    }
+}
---
+++ Utils/Utilities.cs
@@ -0,0 +1,27 @@
+﻿namespace CreateFactor.Utils
+{
+    public class Utilities
+    {
+        private readonly IWebHostEnvironment environment;
+        public Utilities(IWebHostEnvironment environment)
+        {
+            this.environment = environment;
+        }
+        public bool ByteArrayToFile(string fileName, byte[] byteArray)
+        {
+            try
+            {
+                using (var fs = new FileStream(Path.Combine(environment.WebRootPath, fileName), FileMode.Create, FileAccess.Write))
+                {
+                    fs.Write(byteArray, 0, byteArray.Length);
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in process: {0}", ex);
+                return false;
+            }
+        }
+    }
+}
